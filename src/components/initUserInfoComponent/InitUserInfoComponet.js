80ada58049bd05655437fa107c105cc0e489cb5d
laderben <z874154748@sina.com>
2023-04-26 17:23:11 -0400

    add loading process

--- src/components/initUserInfoComponent/InitUserInfoComponet.js
+++ src/components/initUserInfoComponent/InitUserInfoComponet.js
@@ -3,6 +3,7 @@ import ProfileContainer from "../profileContainer/ProfileContainer";
 
 export default function InitUserInfoComponent(props) {
     const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
         fetchData();
@@ -10,14 +11,17 @@ export default function InitUserInfoComponent(props) {
     
 
     async function fetchData() {
+        setLoading(true);
         const response = await fetch("https://reqres.in/api/users?delay=3");
         const json = await response.json();
         setData(json.data);
+        setLoading(false);
     }
 
     return (
         <div>
                 {
+                    loading? <h1>Loading</h1>:
                     data.map(user => (<ProfileContainer user={user}/>))
                 }
         </div>
