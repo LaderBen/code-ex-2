f879f960c13aa063acfe54e758feb06a53d6221b
laderben <z874154748@sina.com>
2023-04-26 21:01:34 -0400

    new loading feature

--- src/components/initUserInfoComponent/InitUserInfoComponet.js
+++ src/components/initUserInfoComponent/InitUserInfoComponet.js
@@ -1,39 +1,48 @@
 import { useEffect, useState } from "react";
 import './style.css'
+import LoadingComponent from "../loadingComponent/LoadingComponent";
 
 export default function InitUserInfoComponent(props) {
     const [data, setData] = useState([]);
-    const [loading, setLoading] = useState(false);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         fetchData();
-    }, [])
+    })
+
+    useEffect(() => {
+        console.log(new Date());
+    }, [data])
 
 
     async function fetchData() {
-        setLoading(true);
         const response = await fetch("https://reqres.in/api/users?delay=3");
         const json = await response.json();
         setData(json.data);
         setLoading(false);
     }
 
+    const loadingComponent = [];
+
+    for (let i = 0; i < 6; i++) {
+        loadingComponent.push(<LoadingComponent/>);
+    }
 
     return (
         <div>
             {
-                data.map(user => (
-                    <div className="container">
-                        <div className='photo'><img src={loading ? "/public/logo192.png" : user.avatar} alt="" /></div>
-                        <div className='info'>
-                            <p>First Name: {loading ? "loading" : user.first_name} </p>
-                            <p>Last Name:   {loading ? "loading" : user.last_name}</p>
-                            <p>Email:   {loading ? "loading" : user.email}</p>
+                loading ? loadingComponent : data.map(user => (
+                        <div className="container">
+                            <div className='photo'><img src={loading ? "/public/logo192.png" : user.avatar} alt="" /></div>
+                            <div className='info'>
+                                <p>First Name: {loading ? "loading" : user.first_name} </p>
+                                <p>Last Name:   {loading ? "loading" : user.last_name}</p>
+                                <p>Email:   {loading ? "loading" : user.email}</p>
+                            </div>
                         </div>
-                    </div>
 
 
-                ))
+                    ))
             }
         </div>
     )
---
+++ src/components/loadingComponent/LoadingComponent.js
@@ -0,0 +1,12 @@
+export default function LoadingComponent(){
+    return (
+        <div className="container">
+            <div className='photo'><img src="https://media.tenor.com/je-huTL1vwgAAAAi/loading-buffering.gif" alt="" /></div>
+            <div className='info'>
+                <p>First Name: Loading </p>
+                <p>Last Name:   Loading</p>
+                <p>Email:   Loading</p>
+            </div>
+        </div>
+    )
+}
